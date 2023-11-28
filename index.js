9b160a3d08096497ed8d919e19d5f2943f42b252
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-11-28 09:56:01 -0500

    shopping cart

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // the multiplication function returns the product of two numbers 
 // a function called "concatOdds" takes two arrays of integers as arguments 
+
+// if the shopping cart is empty, return "please add items to the shopping cart"
