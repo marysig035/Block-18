baefa278fa84c5133b5d8a45709294c3ae3c82da
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-12-01 22:40:49 -0500

    added more prompts

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// the multiplication function returns the product of two numbers 
-// a function called "concatOdds" takes two arrays of integers as arguments 
+// Expect multiplication(3, 4) to be a number 
+// Expect multiplication(3, 4) to be equal to 12
+// Expect multiplication("a", 4) to be an error 
 
-// if the shopping cart is empty, return "please add items to the shopping cart"
+// Expect concatOdds([array1], [array2]) should return a single array only containing the odds numbers in ascending order
+
+// When a user checksout their shopping cart the user is asked to checkout as a guest or as a logged-in user 
+// if the cart is empty return the promt "please enter some items into the shopping cart to proceed"
+// at each step of the checkout process, show the price of the item being scanned 
