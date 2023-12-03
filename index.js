274476f6985ec81d2ebc05752df28e193dcf4bc3
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-12-03 18:23:39 -0500

    added more unit tests

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // Expect multiplication(3, 4) to be equal to 12
 // Expect multiplication("a", 4) to be an error 
 
-// Expect concatOdds([array1], [array2]) should return a single array only containing the odds numbers in ascending order
+// Expect concatOdds([array1], [array2]) to return a single array only containing the odd numbers in ascending order
+// Expect concatOdds([5, 8, 2, 3], [0, 5, 7, 6, 2]) to return [3, 5, 5, 7]
+// Expect concatOdds([1, 2], [5, 4, 3, 2, 1]) to return [1, 1, 3, 5]
+// Expect concatOdds([1, "a"], [5, 4, 3, 2, 1]) to return an error
 
-// When a user checksout their shopping cart the user is asked to checkout as a guest or as a logged-in user 
+// When a user checks out their shopping cart the user is asked to checkout as a guest or as a logged-in user 
 // if the cart is empty return the promt "please enter some items into the shopping cart to proceed"
 // at each step of the checkout process, show the price of the item being scanned 
